41f98b0c096d6109ec676e1f9dc1bda697e13efb
bobby grayson <bobbygrayson@gmail.com>
2015-07-04 15:36:22 -0400

    extremely horrible implementation of atom functionality

--- lib/ribby.rb
+++ lib/ribby.rb
@@ -9,4 +9,35 @@ module Ribby
     chars.gsub!(')', ' ) ')
     chars.split ' '
   end
+
+  def atom(token)
+    # numbers become numbers. all else becomes a symbol
+    begin
+      check_zero_int(token)
+    rescue ValueError
+      begin
+        check_zero_float(token)
+      rescue ValueError
+        token.to_sym
+      end
+    end
+  end
+
+  # these will later become monkeypatches
+  class ValueError < StandardError; end
+  def check_zero_int(num)
+    if num == '0' || num == 0
+      num.to_i
+    else
+      raise ValueError
+    end
+  end
+
+  def check_zero_float(num)
+    if num == '0.0' || num == 0.0
+      num.to_f
+    else
+      raise ValueError
+    end
+  end
 end
--- spec/ribby_spec.rb
+++ spec/ribby_spec.rb
@@ -10,5 +10,15 @@ describe Ribby do
       input = "(+ 2 3 4)"
       expect(Ribby.tokenize(input)).to eq ["(", "+", "2", "3", "4", ")"]
     end
+
+    it 'can create atoms from tokens' do
+      # numbers should become numbers, everything else should become symbols
+      val_1 = '0'
+      val_2 = '0.0'
+      val_3 = 'f'
+      expect(Ribby.atom(val_1)).to eq 0
+      expect(Ribby.atom(val_2)).to eq 0.0
+      expect(Ribby.atom(val_3)).to eq :f
+    end
   end
 end
